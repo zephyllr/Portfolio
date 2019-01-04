365344bac0bc41e6fde73a57194fa8a097c90491
Alice Lu <lalice72098@yahoo.com>
2019-01-03 22:16:26 -0500

    Updates

---
+++ precache-manifest.eac3f6fe507a71f4375a0223cfd2ed02.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fbd11f36e880540307b8",
+    "url": "/static/js/main.fbd11f36.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "e7fd0cf3c7ee0ac51d361633d4a2df73",
+    "url": "/index.html"
+  }
+];
